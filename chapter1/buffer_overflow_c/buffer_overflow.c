0b801ab8f533d23a02234702efd70ce48ba2d970
John Ward <john@johnward.net>
2021-11-13 10:57:43 +0000

    Altered Bufferoverflow example

--- chapter1/buffer_overflow_c/buffer_overflow.c
+++ chapter1/buffer_overflow_c/buffer_overflow.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-  
+
 int main(int argc, char *argv[])
 {
-  
+
        // Reserve 5 byte of buffer plus the terminating NULL.
        // should allocate 8 bytes = 2 double words,
        // To overflow, need more than 8 bytes...
-       char buffer[6];  // If more than 8 characters input
-                        // by user, there will be access 
+       char buffer[10]; // If more than 8 characters input
+                        // by user, there will be access
                         // violation, segmentation fault
-  
+       int password = 0;
+
        // a prompt how to execute the program...
        if (argc < 2)
        {
@@ -19,14 +20,31 @@ int main(int argc, char *argv[])
               printf("Syntax: %s <characters>\n", argv[0]);
               exit(0);
        }
-  
+
        // copy the user input to mybuffer, without any
        // bound checking a secure version is srtcpy_s()
+
        strcpy(buffer, argv[1]);
-       printf("buffer content= %s\n", buffer);
-  
+
+       if (strcmp(buffer, "mypassword"))
+       {
+              printf("Password is wrong\n\r");
+       }
+       else
+       {
+              printf("Password is correct\n\r");
+              password = 1;
+       }
+
+       if (password)
+       {
+              printf("Correct password entered, secret information accessed!");
+       }
+
+       //strcpy(buffer, argv[1]);
+       //printf("buffer content= %s\n", buffer);
+
        // you may want to try strcpy_s()
-       printf("strcpy() executed...\n");
-  
+
        return 0;
 }
