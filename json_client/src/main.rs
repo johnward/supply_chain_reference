492f88ac6b2e4993c6f44c06941ec2b8cb0aa3b6
John Ward <john@johnward.net>
2022-01-24 09:09:43 +0000

    Hyper client to test server

--- json_client/src/main.rs
+++ json_client/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
 
     if arg == "create" {
         create();
+    } else if arg == "list_orders" {
+        list_orders();
     }
 }
 
@@ -20,13 +22,13 @@ async fn create() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .header("content-type", "application/json")
         .body(Body::from(
             r#"{"id":0,
-                "product_name":"Harry Potter",
-                "product_id":3,
-                "customer_id": 5,
-                "amount":3,
-                "address":"4 Privot Drive, London",
-                "fulfilled":false
-            }"#,
+            "product_name":"Harry Potter",
+            "product_id":3,
+            "customer_id": 5,
+            "amount":3,
+            "address":"4 Privot Drive, London",
+            "fulfilled":false
+        }"#,
         ))?;
 
     let client = Client::new();
@@ -34,6 +36,31 @@ async fn create() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let resp = client.request(req).await?;
 
     println!("Response: {}", resp.status());
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn list_orders() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Still inside `async fn main`...
+    //let client = Client::new();
+
+    // // Parse an `http::Uri`...
+    // let uri = "http://localhost:8080/order/list/5".parse()?;
+
+    // // Await the response...
+    // let mut resp = client.get(uri).await?;
+
+    let req = Request::builder()
+        .method(Method::GET)
+        .uri("http://localhost:8080/order/list/5")
+        .header("content-type", "application/json").body(Body::from(r#""#))?;
+
+    let client = Client::new();
+
+    let resp = client.request(req).await?;
+
+    println!("Response: {:?}", resp.status());
 
     Ok(())
 }
