593e3d49dbc0efdd3b4fe9575a441b574d55df2f
John Ward <john@johnward.net>
2022-02-22 20:45:27 +0000

    Removed Base.rs

--- rust_web_service/src/data/base.rs
+++
@@ -1,6 +0,0 @@
-// trait Crud<T> {
-//     fn create(conn: &PgConnection, order: &'a T) -> T;
-//     fn update(conn: &PgConnection, order: &'a T) -> T;
-//     fn delete(conn: &PgConnection, order: &'a T) -> T;
-//     fn show(conn: &PgConnection, order: &'a T) -> T;
-// }
