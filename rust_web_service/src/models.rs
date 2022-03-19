a1eade2caaf464877eebfeff4a0b0e397123f1a9
John Ward <john@johnward.net>
2022-03-19 22:45:29 +0000

    Reverted orders

--- rust_web_service/src/models.rs
+++ rust_web_service/src/models.rs
@@ -15,10 +15,11 @@ pub struct OrderRow {
 #[derive(Clone, Queryable, AsChangeset, Debug, Serialize, Deserialize)]
 pub struct Order {
     pub id: i32,
+    pub product_name: String,
+    pub product_id: i32,
     pub customer_id: i32,
     pub amount: i32,
     pub address: String,
-    pub fulfilled: bool,
 }
 
 // impl Order {
